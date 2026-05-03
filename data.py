9189d7dc61e3d2dd180417e7f956f2e486a4fcac
mcozkan <mcozkan.ds@gmail.com>
2026-05-04 02:26:59 +0300

    build: finalize dvc setup and data tracking mcoz

--- data.py
+++ data.py
@@ -2,15 +2,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from transformers import pipeline
-
-pipe = pipeline("text-classification", model="ProsusAI/finbert")
-
+import os
 import kagglehub
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 # Download latest version
 path = kagglehub.dataset_download("raghadalharbi/all-products-available-on-sephora-website")
 
 print("Path to dataset files:", path)
-print(pipe)
+
+data = os.listdir(path)
+
+df = pd.read_csv(os.path.join(path, data[0]))
+df.to_csv("data/raw"+ "/" + data[0], index=False)
+print(df.head())
+
