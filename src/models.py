10778178591d674dae9e490eb1ec2ddcad3af35e
mcozkan <mcozkan.ds@gmail.com>
2026-05-09 04:58:56 +0300

    artifacts/docs, docker-compose.yaml, requirements.txt, src folder created and model.py, main.py, db_operations.py created and moved to under src/ folder mcoz

---
+++ src/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Optional, List
+from sqlmodel import SQLModel, Field, Column
+from sqlalchemy import JSON
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "user":
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    email: str
+    password: str
+    active: bool
+    created_at: datetime
+    updated_at: datetime
+    
+
